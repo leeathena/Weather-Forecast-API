a943533aeebfabbb626b50ac5a712577edc7d61b
athena lee <hoyan.la@gmail.com>
2024-01-13 03:45:30 +0800

    5 days forecast added

--- starter/script.js
+++ starter/script.js
@@ -9,41 +9,57 @@ document.getElementById('search-button').addEventListener('click', function(e) {
     fetch(geocodeUrl)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         if (data.length === 0) {
             throw new Error('City not found');
         }
 
         const cityLon = data[0].lon;
         const cityLat = data[0].lat;
-
         const queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${cityLat}&lon=${cityLon}&appid=${APIKey}`;
 
         return fetch(queryURL);
     })
     .then(response => response.json())
     .then(cityData => {
-        console.log(cityData)
+        displayCurrentWeather(cityData, cityName); // Display current weather
 
-        // Now you can use cityData here
-        $(".city").text(cityData.city.name + " Weather Details");
-        $(".today").text(dayjs().format('dddd, MMMM D'));
-        $(".wind").text("Wind Speed: " + cityData.list[0].wind.speed);
-        $(".humidity").text("Humidity: " + cityData.list[0].main.humidity);
-
-        // Convert the temp to Celsius
-        const tempC = cityData.list[0].main.temp - 273.15;
-
-        // Add temp content to html
-        $(".temp").text("Temperature (K) " + cityData.list[0].main.temp);
-        $(".tempC").text("Temperature (C) " + tempC.toFixed(2));
-
-        // Log the data in the console
-        console.log("Wind Speed: " + cityData.list[0].wind.speed);
-        console.log("Humidity: " + cityData.list[0].main.humidity);
-        console.log("Temperature (C): " + tempC);
+        //5 day forecast display
+        displayFiveDayForecast(cityData); // Display 5-day forecast
     })
     .catch(error => {
         console.error('Error:', error);
     });
 });
+
+function displayCurrentWeather(cityData, cityName) {
+    $(".city").text(cityName + " " + dayjs().format('dddd, MMMM D'));
+    $(".wind").text("Wind Speed: " + cityData.list[0].wind.speed + " m/s");
+    $(".humidity").text("Humidity: " + cityData.list[0].main.humidity + "%");
+    const tempC = cityData.list[0].main.temp - 273.15;
+    $(".temp").text("Temperature (C): " + tempC.toFixed(2));
+}
+
+function displayFiveDayForecast(cityData) {
+    $('#forecast-container').empty(); // Clear previous forecast
+    const fiveDayForecast = [8, 16, 24, 32, 39];
+    $.each(fiveDayForecast, function(i, index) {
+        const forecast = cityData.list[index];
+        const dateString = forecast.dt_txt.split(' ')[0];
+        const iconUrl = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+        const tempCelsius = forecast.main.temp - 273.15;
+        displayDayForecast(dateString, iconUrl, tempCelsius, forecast.wind.speed, forecast.main.humidity);
+    });
+}
+
+function displayDayForecast(date, icon, temp, wind, humidity) {
+    const forecastElement = `
+        <div class="weather-card">
+            <h3>${date}</h3>
+            <img src="${icon}" alt="Weather Icon">
+            <p>Temperature: ${temp.toFixed(2)} °C</p>
+            <p>Wind: ${wind} m/s</p>
+            <p>Humidity: ${humidity}%</p>
+        </div>
+    `;
+    $('#forecast-container').append(forecastElement);
+}
